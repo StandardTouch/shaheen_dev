76b75dc6191ace9283771a85a0c741ffe7aef4fa
zaid2229 <zzaidu2018@gmail.com>
2024-08-09 23:20:17 +0530

    add depende

--- shaheen_dev/public/js/generate_certificate.js
+++ shaheen_dev/public/js/generate_certificate.js
@@ -1,20 +1,20 @@
 frappe.ui.form.on('Student Complete Progress', {
     refresh: function(frm) {
+        // Add a custom button to the form view
         console.log('hiiiiiiiiiiiiiiiii')
-
-                frappe.call({
-                    method: "shaheen_dev.api.generate_certificate.generate_certificate",
-                    args: {
-                        docname: frm.doc.name
-                    },
-                    callback: function(r) {
-                        if (r.message) {
-                            frappe.msgprint(__('Certificate generated successfully!'));
-                            frm.reload_doc();
-                        }
+        frm.add_custom_button(('Generate Certificate'), function() {
+            frappe.call({
+                method: "shaheen_dev.api.generate_certificate.generate_certificate",
+                args: {
+                    docname: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frappe.msgprint(__('Certificate generated successfully!'));
+                        frm.reload_doc();  // Reload the document to reflect any changes
                     }
-                });
-         
-        }
-  
+                }
+            });
+        });
+    }
 });
